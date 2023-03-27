a542cae9618359ceaaf3d2637f2425202b65252d
Daniel Wood <109208162+danwood1992@users.noreply.github.com>
2023-03-27 14:37:19 +0100

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-
 path('profiles/', views.staff_Profiles, name="profiles"),
 path('upload-cv/', views.cvUpload, name='upload-cv'),
 ]
