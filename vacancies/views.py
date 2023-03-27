2bcf898db000e3636c7dc67a7deecc15748d8819
Daniel Wood <109208162+danwood1992@users.noreply.github.com>
2023-03-27 14:38:22 +0100

    Update views.py

--- vacancies/views.py
+++ vacancies/views.py
@@ -5,14 +5,12 @@ from django.contrib import messages
 from .models import Vacancy, Tag, Salary
 
 def vacancies(request):
-
     vacancies = Vacancy.objects.all()
     context = {'vacancies': vacancies}
     return render(request, 'vacancies.html', context)
 
 def vacancy(request, pk):
     vacancyObj = Vacancy.objects.get(id=pk)
-
     return render(request, 'single-vacancy.html')
 
 
