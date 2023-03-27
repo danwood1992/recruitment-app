8143cd5dc37cd79e368b5b2c2ef77c719249cf4d
Daniel Wood <109208162+danwood1992@users.noreply.github.com>
2023-03-27 14:38:05 +0100

    Update urls.py

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   
-    
     path('vacancy/<str:pk>/', views.vacancy, name="vacancy"),
-   
     path('vacancies/', views.vacancies, name='vacancies'),
-
 ]
